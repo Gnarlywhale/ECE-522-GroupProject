5cb82954dd8a8d870fd5bb8ee7aa3b87796c4b9c
kevinx231 <kevinx231@gmail.com>
2023-11-07 12:45:57 -0700

    correction to rotation mistake

--- RB_Tree/src/main.rs
+++ RB_Tree/src/main.rs
@@ -1,6 +1,6 @@
+use binary_lib::*;
 use std::cell::RefCell;
 use std::rc::Rc;
-use binary_lib::*;
 
 #[derive(Clone, Debug, PartialEq)]
 enum NodeColor {
@@ -16,6 +16,7 @@ type Tree = Rc<RefCell<TreeNode<u32>>>;
 type RedBlackTree = Option<Tree>;
 
 #[derive(Debug)]
+#[derive(PartialEq)]
 struct TreeNode<T> {
     pub color: NodeColor,
     pub key: T,
@@ -24,8 +25,8 @@ struct TreeNode<T> {
     right: RedBlackTree,
 }
 
-impl <T: Ord> TreeNode<T>{
-    fn new(data:T)-> Self {
+impl<T: Ord> TreeNode<T> {
+    fn new(data: T) -> Self {
         Self {
             color: NodeColor::Red,
             key: data,
@@ -43,11 +44,11 @@ impl <T: Ord> TreeNode<T>{
 // }
 // impl RBTree {
 //     pub fn new() -> Self {
-//         RBTree { root: None } 
+//         RBTree { root: None }
 //     }
 
 //     pub fn insert_node(&mut self, data:T){
-        
+
 //     }
 // }
 
@@ -55,10 +56,10 @@ fn new_rb_tree(data: u32) -> RedBlackTree {
     Some(Rc::new(RefCell::new(TreeNode::new(data))))
 }
 
-fn find_key(rb_tree:RedBlackTree, key: u32) -> Option<Rc<RefCell<TreeNode<u32>>>>{
+fn find_key(rb_tree: RedBlackTree, key: u32) -> Option<Rc<RefCell<TreeNode<u32>>>> {
     if let Some(node) = rb_tree {
         if key == node.borrow_mut().key {
-            return Some(node)
+            return Some(node);
         } else if key < node.borrow_mut().key {
             let left = node.borrow_mut().left.take();
             return find_key(left, key);
@@ -70,10 +71,10 @@ fn find_key(rb_tree:RedBlackTree, key: u32) -> Option<Rc<RefCell<TreeNode<u32>>>
     None
 }
 
-fn insert_node(rb_tree:RedBlackTree, data: u32) -> RedBlackTree{
+fn insert_node(rb_tree: RedBlackTree, data: u32) -> RedBlackTree {
     if let Some(node) = rb_tree {
-        if data == node.borrow().key{
-            return None
+        if data == node.borrow().key {
+            return None;
         }
         if data < node.borrow().key {
             let left = node.borrow_mut().left.take();
@@ -96,7 +97,7 @@ fn insert_node(rb_tree:RedBlackTree, data: u32) -> RedBlackTree{
     }
 }
 
-fn get_child(opt_node:RedBlackTree,direction:Direction) -> RedBlackTree {
+fn get_child(opt_node: RedBlackTree, direction: Direction) -> RedBlackTree {
     if let Some(node) = opt_node {
         match direction {
             Direction::Right => return node.borrow().right.clone(),
@@ -106,50 +107,68 @@ fn get_child(opt_node:RedBlackTree,direction:Direction) -> RedBlackTree {
     None
 }
 
-fn left_rotate(y: RedBlackTree) -> Result<RedBlackTree, ()>{
+fn left_rotate(y: RedBlackTree) -> Result<RedBlackTree, ()> {
     let node_y = y.clone();
-    if let Some(y_node) = y{
+    if let Some(y_node) = y {
         let z = y_node.borrow_mut().parent.take();
         let x = y_node.borrow_mut().right.take();
-        y_node.borrow_mut().parent= x.clone();
-        if let Some(x_node) = x{
+        y_node.borrow_mut().parent = x.clone();
+        if let Some(x_node) = x {
             let child = x_node.borrow_mut().left.take();
             x_node.borrow_mut().parent = z.clone();
             x_node.borrow_mut().left = node_y.clone();
             y_node.borrow_mut().right = child.clone();
-            if let Some(child_node) = child{
+            if let Some(child_node) = child {
                 child_node.borrow_mut().parent = node_y.clone();
             }
-            Ok(Some(x_node))
-        }
-        else{
+            if let Some(z_node) = z {
+                if z_node.borrow().left == node_y{
+                    z_node.borrow_mut().left = Some(x_node);
+                }
+                else{
+                    z_node.borrow_mut().right = Some(x_node);
+                }
+                Ok(Some(z_node))
+            } else {
+                Ok(Some(x_node))
+            }
+        } else {
             Err(())
         }
-    }else{
+    } else {
         Err(())
     }
 }
 
-fn right_rotate(y: RedBlackTree) -> Result<RedBlackTree, ()>{
+fn right_rotate(y: RedBlackTree) -> Result<RedBlackTree, ()> {
     let node_y = y.clone();
-    if let Some(y_node) = y{
+    if let Some(y_node) = y {
         let z = y_node.borrow_mut().parent.take();
         let x = y_node.borrow_mut().left.take();
-        y_node.borrow_mut().parent= x.clone();
-        if let Some(x_node) = x{
+        y_node.borrow_mut().parent = x.clone();
+        if let Some(x_node) = x {
             let child = x_node.borrow_mut().right.take();
             x_node.borrow_mut().parent = z.clone();
             x_node.borrow_mut().right = node_y.clone();
             y_node.borrow_mut().left = child.clone();
-            if let Some(child_node) = child{
+            if let Some(child_node) = child {
                 child_node.borrow_mut().parent = node_y.clone();
             }
-            Ok(Some(x_node))
-        }
-        else{
+            if let Some(z_node) = z {
+                if z_node.borrow().left == node_y{
+                    z_node.borrow_mut().left = Some(x_node);
+                }
+                else{
+                    z_node.borrow_mut().right = Some(x_node);
+                }
+                Ok(Some(z_node))
+            } else {
+                Ok(Some(x_node))
+            }
+        } else {
             Err(())
         }
-    }else{
+    } else {
         Err(())
     }
 }
@@ -159,7 +178,7 @@ fn right_rotate(y: RedBlackTree) -> Result<RedBlackTree, ()>{
 // 2- Delete a node from the red-black tree.
 // 3- Count the number of leaves in a tree.
 // 4- Return the height of a tree.
-// 5- Print In-order traversal of the tree.  -> Starting from root, then all lefts  
+// 5- Print In-order traversal of the tree.  -> Starting from root, then all lefts
 // 6- Check if the tree is empty.
 // 7- Print the tree showing its colors and structure. (Using println!(“{:#?}”,tree); is NOT
 // sufficient)
@@ -170,12 +189,55 @@ fn main() {
     rb_tree = insert_node(rb_tree, 45);
     rb_tree = insert_node(rb_tree, 55);
     rb_tree = left_rotate(rb_tree).unwrap();
-    println!("{:?}",rb_tree.clone().unwrap().borrow().key);
-    println!("{:?}", get_child(rb_tree.clone(),Direction::Right).unwrap().borrow().key);
-    println!("{:?}", get_child(rb_tree.clone(),Direction::Left).unwrap().borrow().key);
-    println!("{:?}",rb_tree.clone().unwrap().borrow().right.clone().unwrap().borrow().key);
-    println!("{:?}",rb_tree.clone().unwrap().borrow().left.clone().unwrap().borrow().key);
-    println!("{:?}",get_child(get_child(rb_tree.clone(),Direction::Left),Direction::Right).unwrap().borrow().key);
+    println!("{:?}", rb_tree.clone().unwrap().borrow().key);
+    println!(
+        "{:?}",
+        get_child(rb_tree.clone(), Direction::Right)
+            .unwrap()
+            .borrow()
+            .key
+    );
+    println!(
+        "{:?}",
+        get_child(rb_tree.clone(), Direction::Left)
+            .unwrap()
+            .borrow()
+            .key
+    );
+    println!(
+        "{:?}",
+        rb_tree
+            .clone()
+            .unwrap()
+            .borrow()
+            .right
+            .clone()
+            .unwrap()
+            .borrow()
+            .key
+    );
+    println!(
+        "{:?}",
+        rb_tree
+            .clone()
+            .unwrap()
+            .borrow()
+            .left
+            .clone()
+            .unwrap()
+            .borrow()
+            .key
+    );
+    println!(
+        "{:?}",
+        get_child(
+            get_child(rb_tree.clone(), Direction::Left),
+            Direction::Right
+        )
+        .unwrap()
+        .borrow()
+        .key
+    );
 
     let val = find_key(rb_tree, 50);
     println!("{:?}", val.unwrap().borrow().key);
@@ -185,30 +247,28 @@ fn main() {
     //     println!("Boo")
     // }
     // println!("{:?}", f.clone().unwrap().borrow().key)
-    
 }
 
 // impl <T: Ord> TreeNode<T>{
-    // fn new(data:T)-> Self {
-    // fn new(data:T, node_parent:Option<TreeNode<u32>>)-> Self {
-        // if let Some(np) = node_parent{
-        //     Self {
-        //         color: NodeColor::Red,
-        //         key: data,
-        //         parent: Some(Rc::new(RefCell::new(node_parent.unwrap()))),
-        //         left: None,
-        //         right: None,
-        //     }
-        // } else {
-    //         Self {
-    //             color: NodeColor::Red,
-    //             key: data,
-    //             parent: None,
-    //             left: None,
-    //             right: None,
-    //         }
-        // }
-
-    // } 
+// fn new(data:T)-> Self {
+// fn new(data:T, node_parent:Option<TreeNode<u32>>)-> Self {
+// if let Some(np) = node_parent{
+//     Self {
+//         color: NodeColor::Red,
+//         key: data,
+//         parent: Some(Rc::new(RefCell::new(node_parent.unwrap()))),
+//         left: None,
+//         right: None,
+//     }
+// } else {
+//         Self {
+//             color: NodeColor::Red,
+//             key: data,
+//             parent: None,
+//             left: None,
+//             right: None,
+//         }
 // }
 
+// }
+// }
