4c9c9626cbcbf214e0b964063529537a3d690020
kevinx231 <kevinx231@gmail.com>
2023-11-27 20:46:11 -0700

    rb delete complete (still need further testing)

--- RB_Tree/src/main.rs
+++ RB_Tree/src/main.rs
@@ -2,6 +2,7 @@ use binary_lib::*;
 use colored::*;
 use std::cell::RefCell;
 use std::env;
+use std::f32::consts::PI;
 use std::rc::Rc;
 
 #[derive(Clone, Debug, PartialEq)]
@@ -96,29 +97,45 @@ fn in_order_successor(rb_tree: RedBlackTree) -> RedBlackTree {
 // remove node returns the tree root and a pointer to the newly inserted node (also a tree)
 
 // TODO double check edge cases! i.e. 1 node tree
-fn remove_node(mut rb_tree: RedBlackTree, data: u32) -> (RedBlackTree, RedBlackTree) {
+fn remove_node(
+    mut rb_tree: RedBlackTree,
+    data: u32,
+) -> (
+    RedBlackTree,
+    RedBlackTree,
+    NodeColor,
+    RedBlackTree,
+    RedBlackTree,
+    Direction,
+) {
+    let mut color = NodeColor::Black;
     let mut replacement_node: Option<Rc<RefCell<TreeNode<u32>>>> = None;
+    let mut parent_node: Option<Rc<RefCell<TreeNode<u32>>>> = None;
+    let (mut sibling, mut sibling_direction) = get_sibling(find_key(rb_tree.clone(), data));
     if let Some(node) = find_key(rb_tree.clone(), data) {
         // Handle terminal node case
         let node_key = node.borrow().key.clone();
-        
+
         if node.borrow().left.is_none() && node.borrow().right.is_none() {
+            color = node.clone().borrow().color.clone();
             let parent = &node.borrow().parent;
+            parent_node = parent.clone();
             if let Some(p_node) = parent {
                 if node_key < p_node.borrow().key {
                     p_node.borrow_mut().left = None;
                 } else {
                     p_node.borrow_mut().right = None;
                 }
-                return (rb_tree, None) ;
             } else {
-                return (None, None);
+                return (None, None, NodeColor::Black, None, None, Direction::Root);
             }
         } else if node.borrow().left.is_none() && node.borrow().right.is_some() {
             // One right child
+            color = node.clone().borrow().color.clone();
             let rep_node = node.borrow().right.clone();
             replacement_node = rep_node.clone();
             let parent = node.borrow().parent.clone();
+            parent_node = parent.clone();
             if let Some(p_node) = parent {
                 if node_key < p_node.borrow().key {
                     p_node.borrow_mut().left = rep_node.clone();
@@ -133,9 +150,11 @@ fn remove_node(mut rb_tree: RedBlackTree, data: u32) -> (RedBlackTree, RedBlackT
             }
         } else if node.borrow().left.is_some() && node.borrow().right.is_none() {
             // One left child
-             let rep_node = node.borrow().left.clone();
-             replacement_node = rep_node.clone();
+            color = node.clone().borrow().color.clone();
+            let rep_node = node.borrow().left.clone();
+            replacement_node = rep_node.clone();
             let parent = node.borrow().parent.clone();
+            parent_node = parent.clone();
             if let Some(p_node) = parent {
                 if node_key < p_node.borrow().key {
                     p_node.borrow_mut().left = rep_node.clone();
@@ -149,12 +168,16 @@ fn remove_node(mut rb_tree: RedBlackTree, data: u32) -> (RedBlackTree, RedBlackT
                 node.borrow_mut().right = None
             }
         } else {
-            if let Some(rep_node) = in_order_successor(Some(node.clone())) {
+            let sucessor = in_order_successor(Some(node.clone()));
+            (sibling, sibling_direction) = get_sibling(sucessor.clone());
+            if let Some(rep_node) = sucessor.clone() {
+                color = rep_node.clone().borrow().color.clone();
                 // Set the parent key to match the rep_node key
-                replacement_node = Some(rep_node.clone());
                 let temp_key = rep_node.clone().borrow().key;
+                println!("{:?}", temp_key.clone());
                 // Remove the reference from the replacement node's parent to the replacement node
                 let parent = &rep_node.borrow().parent;
+                parent_node = parent.clone();
                 if let Some(p_node) = parent {
                     if rep_node.clone().borrow().key < p_node.clone().borrow().key {
                         p_node.clone().borrow_mut().left = None;
@@ -167,8 +190,192 @@ fn remove_node(mut rb_tree: RedBlackTree, data: u32) -> (RedBlackTree, RedBlackT
             }
         }
     }
-    
-    return (rb_tree, replacement_node);
+
+    return (
+        rb_tree,
+        replacement_node,
+        color,
+        parent_node,
+        sibling,
+        sibling_direction,
+    );
+}
+
+fn delete(rb_tree: RedBlackTree, data: u32) -> RedBlackTree {
+    let v = find_key(rb_tree.clone(), data);
+    let mut v_color = NodeColor::Black;
+    if let Some(node) = v {
+        v_color = node.borrow().color.clone();
+    }
+    let (new_tree, replacement, color, parent, sibling, sibling_direction) =
+        remove_node(rb_tree, data);
+    if new_tree.is_some() {
+        let root = delete_balance(replacement, color, parent, sibling, sibling_direction);
+        if root.is_some(){
+            return root;
+        }
+    }
+    return new_tree;
+}
+
+fn delete_balance(replacement: RedBlackTree, org_color: NodeColor, parent: RedBlackTree, sibling: RedBlackTree, sibling_direction: Direction) -> RedBlackTree {
+    if let Some(r_node) = replacement.clone() {
+        //simple recolor case
+        if r_node.borrow().color == NodeColor::Red
+            || org_color == NodeColor::Red
+            || r_node.borrow().parent.is_none()
+        {
+            println!("simple case");
+            r_node.borrow_mut().color = NodeColor::Black;
+            return None;
+        }
+    }
+    //simple recolor case
+    else {
+        if org_color == NodeColor::Red {
+            println!("simple case None replacement");
+            return None;
+        }
+    }
+    if let Some(s_node) = sibling.clone() {
+        let mut s_right_color = NodeColor::Black;
+        let mut s_left_color = NodeColor::Black;
+        if let Some(s_right) = s_node.clone().borrow().right.clone() {
+            s_right_color = s_right.borrow().color.clone();
+        }
+        if let Some(s_left) = s_node.clone().borrow().left.clone() {
+            s_left_color = s_left.borrow().color.clone();
+        }
+        if s_node.borrow().color == NodeColor::Black {
+            if s_right_color == NodeColor::Black && s_left_color == NodeColor::Black {
+                //recur for parent
+                if let Some(p_node) = parent.clone(){
+                    if p_node.clone().borrow().color == NodeColor::Red{
+                        println!("red parent");
+                        p_node.clone().borrow_mut().color = NodeColor::Black;
+                        s_node.clone().borrow_mut().color = NodeColor::Red;
+                        return None;
+                    }else{
+                        println!("recursive case");
+                        s_node.clone().borrow_mut().color = NodeColor::Red;
+                        let (new_s, new_s_direction) = get_sibling(parent.clone());
+                        let new_parent = p_node.clone().borrow().parent.clone();
+                        return delete_balance(parent, NodeColor::Black, new_parent, new_s, new_s_direction);
+                    }
+                }
+            } else {
+                match sibling_direction {
+                    Direction::Left => {
+                        if s_left_color == NodeColor::Red {
+                            println!("left left case");
+                            //left left case
+                            let mut p_color = NodeColor::Black;
+                            if let Some(p_node) = parent.clone(){
+                                p_color = p_node.clone().borrow().color.clone();
+                                p_node.clone().borrow_mut().color = NodeColor::Black;
+                            }
+                            if let Some(s_left) = s_node.clone().borrow().left.clone() {
+                                s_left.clone().borrow_mut().color = NodeColor::Black;
+                            }
+                            s_node.clone().borrow_mut().color = p_color;
+                            return right_rotate(&parent);
+                        } else if s_right_color == NodeColor::Red {
+                            println!("left right case");
+                            //left right case
+                            let mut p_color = NodeColor::Black;
+                            if let Some(p_node) = parent.clone(){
+                                p_color = p_node.clone().borrow().color.clone();
+                                p_node.clone().borrow_mut().color = NodeColor::Black;
+                            }
+                            if let Some(s_right) = s_node.clone().borrow().right.clone() {
+                                s_right.clone().borrow_mut().color = NodeColor::Black;
+                            }
+                            s_node.clone().borrow_mut().color = p_color;
+                            left_rotate(&sibling);
+                            return right_rotate(&parent);
+                        }
+                    }
+                    Direction::Right => {
+                        if s_right_color == NodeColor::Red {
+                            println!("right right case");
+                            //right right case
+                            let mut p_color = NodeColor::Black;
+                            if let Some(p_node) = parent.clone(){
+                                p_color = p_node.clone().borrow().color.clone();
+                                p_node.clone().borrow_mut().color = NodeColor::Black;
+                            }
+                            if let Some(s_right) = s_node.clone().borrow().right.clone() {
+                                s_right.clone().borrow_mut().color = NodeColor::Black;
+                            }
+                            s_node.clone().borrow_mut().color = p_color;
+                            return left_rotate(&parent);
+                        } else if s_left_color == NodeColor::Red {
+                            println!("right left case");
+                            //right left case
+                            let mut p_color = NodeColor::Black;
+                            if let Some(p_node) = parent.clone(){
+                                p_color = p_node.clone().borrow().color.clone();
+                                p_node.clone().borrow_mut().color = NodeColor::Black;
+                            }
+                            if let Some(s_left) = s_node.clone().borrow().left.clone() {
+                                s_left.clone().borrow_mut().color = NodeColor::Black;
+                            }
+                            s_node.clone().borrow_mut().color = p_color;
+                            right_rotate(&sibling);
+                            return left_rotate(&parent);
+                        }
+                    }
+                    Direction::Root => {}
+                }
+            }
+        } else if s_node.borrow().color == NodeColor::Red {
+            match sibling_direction{
+                Direction::Left=>{
+                    println!("red sibling left case");
+                    //left case if sibling is red
+                    let mut new_sibling = None;
+                    if let Some(p_node) = parent.clone(){
+                        p_node.clone().borrow_mut().color = NodeColor::Red;
+                        s_node.clone().borrow_mut().color = NodeColor::Black;
+                    }
+                    right_rotate(&parent);
+                    if let Some(p_node) = parent.clone(){
+                        new_sibling = p_node.clone().borrow().left.clone();
+                    }
+                    return delete_balance(replacement, NodeColor::Black, parent, new_sibling, Direction::Left);
+                }
+                Direction::Right=>{
+                    println!("red sibling right case");
+                    //right case if sibling is red
+                    let mut new_sibling = None;
+                    if let Some(p_node) = parent.clone(){
+                        p_node.clone().borrow_mut().color = NodeColor::Red;
+                        s_node.clone().borrow_mut().color = NodeColor::Black;
+                    }
+                    left_rotate(&parent);
+                    if let Some(p_node) = parent.clone(){
+                        new_sibling = p_node.clone().borrow().right.clone();
+                    }
+                    return delete_balance(replacement, NodeColor::Black, parent, new_sibling, Direction::Right);
+                }
+                Direction::Root=>{}
+            }
+        }
+    } else {
+        //recur for parent
+        println!("recursive no other nodes");
+        if let Some(p_node) = parent.clone(){
+            if p_node.clone().borrow().color == NodeColor::Red{
+                p_node.clone().borrow_mut().color = NodeColor::Black;
+                return None;
+            }else{
+                let (new_s, new_s_direction) = get_sibling(parent.clone());
+                let new_parent = p_node.clone().borrow().parent.clone();
+                return delete_balance(parent, NodeColor::Black, new_parent, new_s, new_s_direction);
+            }
+        }
+    }
+    None
 }
 
 fn insert_node(rb_tree: RedBlackTree, data: u32) -> RedBlackTree {
@@ -201,7 +408,6 @@ fn insert(rb_tree: RedBlackTree, data: u32) -> RedBlackTree {
     let new_tree = insert_node(rb_tree, data);
     let node = &find_key(new_tree.clone(), data);
     let rotates = insert_balance(node);
-    println!("{:#?}", rotates);
     for (direction, key) in rotates {
         match direction {
             Direction::Left => {
@@ -226,7 +432,6 @@ fn insert(rb_tree: RedBlackTree, data: u32) -> RedBlackTree {
 
 fn insert_balance(x: &RedBlackTree) -> Vec<(Direction, u32)> {
     if let Some(x_node) = x {
-        println!("pointer: start balance");
         let mut x_dir = Direction::Left;
         let p = x_node.clone().borrow().parent.clone();
         if p.is_none() {
@@ -255,7 +460,6 @@ fn insert_balance(x: &RedBlackTree) -> Vec<(Direction, u32)> {
                                     p_node.clone().borrow_mut().color = NodeColor::Black;
                                     u_node.borrow_mut().color = NodeColor::Black;
                                     gp_node.borrow_mut().color = NodeColor::Red;
-                                    println!("pointer: recursive");
                                     return insert_balance(&p_node.clone().borrow().parent);
                                 } else {
                                     match x_dir {
@@ -305,7 +509,6 @@ fn insert_balance(x: &RedBlackTree) -> Vec<(Direction, u32)> {
                                     p_node.borrow_mut().color = NodeColor::Black;
                                     u_node.borrow_mut().color = NodeColor::Black;
                                     gp_node.borrow_mut().color = NodeColor::Red;
-                                    println!("pointer: recursive");
                                     return insert_balance(&p_node.clone().borrow().parent);
                                 } else {
                                     match x_dir {
@@ -371,13 +574,13 @@ fn get_sibling(opt_node: RedBlackTree) -> (RedBlackTree, Direction) {
         let parent = &node.borrow().parent;
         if let Some(p_node) = parent {
             if node.borrow().key < p_node.borrow().key {
-                return (p_node.borrow().right.clone(), Direction::Right)
+                return (p_node.borrow().right.clone(), Direction::Right);
             } else {
-                return (p_node.borrow().left.clone(), Direction::Left)
+                return (p_node.borrow().left.clone(), Direction::Left);
             }
         }
     }
-    return (None, Direction::Root)
+    return (None, Direction::Root);
 }
 
 fn left_rotate(y: &RedBlackTree) -> RedBlackTree {
@@ -396,7 +599,6 @@ fn left_rotate(y: &RedBlackTree) -> RedBlackTree {
             }
             if let Some(z_node) = z {
                 let mut left = false;
-                println!("pointer");
                 if let Some(left_node) = &z_node.clone().borrow().left {
                     if left_node.clone().borrow().key == y_node.clone().borrow().key {
                         left = true;
@@ -474,15 +676,15 @@ fn main() {
     // rb_tree = insert(rb_tree, 11);
     // rb_tree = insert(rb_tree, 12);
     // rb_tree = insert(rb_tree, 13);
-     print_tree(&rb_tree, 0);
-    let mut rep_node;
-    (rb_tree,  rep_node) = remove_node(rb_tree, 6);
-    print_tree (&rep_node, 0);
-     // Sibling testing:
+    rb_tree = delete(rb_tree, 6);
+    print_tree(&rb_tree, 0);
+    // let mut rep_node;
+    // (rb_tree, rep_node) = remove_node(rb_tree, 6);
+    // print_tree(&rep_node, 0);
+    // Sibling testing:
     //  let (sibling, sib_direction) = get_sibling(find_key(rb_tree.clone(), 334));
     // print_tree(&sibling,0);
     // println!("{:?}", sib_direction)
-
 }
 
 //
