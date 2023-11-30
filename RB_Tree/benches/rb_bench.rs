ff36887a47bd9bd58daf0d55d1f81998d036a859
GamaWhale <87339308+GamaWhale@users.noreply.github.com>
2023-11-29 19:43:46 -0700

    Put in the proper benchmark values, also increased the duration to allow for 100 samples on my machine (not sure if its worth  the wait tbh)

--- RB_Tree/benches/rb_bench.rs
+++ RB_Tree/benches/rb_bench.rs
@@ -1,19 +1,21 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
+use std::time::Duration;
 use RB_Tree::*;
 
 
 fn criterion_benchmark(c: &mut Criterion){
-    [1000, 10000u32].iter().for_each(|x| {
+    let mut group = c.benchmark_group("RB Benches");
+    group.measurement_time(Duration::from_secs(30));
+    [10000, 40000, 70000, 100000, 130000u32].iter().for_each(|x| {
         let mut rb = new_rb_tree(1);
-        c.bench_function(&format!("{}: {}", "insert", x), |b| {
+        group.bench_function(&format!("{}: {}", "insert", x), |b| {
             b.iter(|| {
                 (2..=*x).into_iter().for_each(|data| {
                    rb =  insert(&rb, black_box(data));
                 });
             });
         });
-        c.bench_function(&format!("{}: {}", "find key", x), |b| {
+        group.bench_function(&format!("{}: {}", "find key", x), |b| {
             b.iter(|| {
                 (1..=*x/10).into_iter().for_each(|data| {
                    _ =  find_key(rb.clone(), black_box(data));
